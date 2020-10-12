5ce769afe89236ee3270689d3473364ed2140657
kluverdev <kluverdev@gmail.com>
2020-10-12 07:13:14 -0400

    kotlin datatypes

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,10 @@
 fun main() {
+    //all data types in kotlin are objects
     println("how are you" +
             "gat")
+    var myInt = 35
+    var myLong = 23L
+    var b1: Byte = Byte.MAX_VALUE;
+    println(b1)
+
 }
