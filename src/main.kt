1fa3f6846faf8463996e9892883a2b1ca4364058
wizzy <kluverdev@gmail.com>
2020-11-07 00:49:25 -0400

    arrays

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,23 @@
 fun main() {
-    //kotlin does not support implicit type conversion eg an int value cannot be
-    //..assigned to  long type but this is possible in java
-    //We use helper functions in kotlin to explicitly convert one type to another
-
-    var long = 200;
-    var convertolong: Long = long.toLong()
-    println(convertolong)
+    val num = arrayOf(1, 2, 3, 4)   //implicit type declaration
+    val num2 = arrayOf<Int>(1, 2, 3) //explicit type declaration
+    println(num.size)
+    for (element in num){
+        println(element)
+    }
+    //using array constructor
+    val num5 = Array(3, {i-> i*1})
+    //using built in factory methods
+    val num4 = intArrayOf(1, 2, 3, 4)
+    //get and set methods of array and also index [] operator...
+    //TRAVERSING
+    for(i in num.indices){
+        println(num[i])
+    }
+    for (i in 0..10){
+        println(i)
+    }
+    num2.forEach({index->println(index)})
+    //Once you create an array, the size is
+    // fixed. You can't add or remove elements, except by copying to a new array.
 }
